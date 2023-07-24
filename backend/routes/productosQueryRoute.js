c795add47abca8b3769b6a57ac3268f542fbf5f8
Jacob0418 <cjosecoronado97@gmail.com>
2023-07-24 11:23:59 -0500

    grht

--- backend/routes/productosQueryRoute.js
+++ backend/routes/productosQueryRoute.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const productosQueryController = require('../controllers/productosQueryController');
-const { route } = require('./categoriasRoutes');
 
 router.get('/', productosQueryController.obtenerProductosQuery);
 
